c9c96e3063d933c3c7afb3cc0e0060a5df53cf17
nzw0301 <nozawa.kento@aist.go.jp>
2017-11-06 16:50:48 +0900

    Update

--- README.md
+++ README.md
@@ -1,9 +1,64 @@
 # DBLP4J
 
-DBLP citation data Parser by Java
+DBLP citation dataset parser by Java
+
+## Run
 
 ```shell
 $ mkdir -p src/main/resources
 $ python download.py
 $ gradle run
 ```
+
+---
+
+## About created files
+
+### `title_year.tsv`
+
+Each line contains 3 fields:
+
+1. paper index (int)
+2. published year (int)
+3. paper title (string)
+
+---
+ 
+### `paper_conference_label.tsv`
+
+Each line contains 2 fields:
+
+1. paper index (int)
+2. conference/journal name (string)
+
+---
+
+### `author_conference_label.tsv`
+
+Each line contains 2 fields:
+
+1. author name (string)
+2. conference/journal names (string, each conference name is split by `TAB` )
+
+---
+
+### `paper_citation_edge.tsv`
+
+Each line contains 2 fields:
+
+1. source paper index (int)
+2. target paper index (int)
+
+Each line represents a directed edge.
+
+---
+
+### `author_citation_edge.tsv`
+
+Each line contains 2 fields:
+
+1. source author name (string)
+2. target author name (string)
+3. the number of citations from source author (int)
+
+Each line represents a weighted and directed edge.
--- src/main/java/dblp4j/DBLP4J.java
+++ src/main/java/dblp4j/DBLP4J.java
@@ -4,44 +4,124 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public final class DBLP4J {
-    private DBLP4J() {};
+    private DBLP4J() {}
     private static final Pattern patternPaperTitle = Pattern.compile("^#\\*");
     private static final Pattern patternYear = Pattern.compile("^#t");
     private static final Pattern patternPaperIndex = Pattern.compile("^#index");
-    private static final Pattern patterCitedPaper = Pattern.compile("^#%");
+    private static final Pattern patternCitedPaper = Pattern.compile("^#%");
+    private static final Pattern patternConference = Pattern.compile("^#c");
+    private static final Pattern patternAuthors = Pattern.compile("^#@");
 
     public static void main(String[] args) throws IOException {
         final String FILENAME = "src/main/resources/dblp-v4.txt";
 
         BufferedReader br = new BufferedReader(new FileReader(FILENAME));
         String line;
+
+        // temporal variable for each paper information
         String paperTitle = "";
-        int publishedYear = 0;
+        String conferenceName = "";
         int paperId = 0;
+        int publishedYear = 0;
+        List<Integer> citationPapers = new ArrayList<>();
+        String[] authors = null;
 
-        FileWriter titleWriter = new FileWriter("titie.txt");
-        FileWriter citationWriter = new FileWriter("citation.txt");
+        Map<String, Set<String>> authorBelongs2Conferences = new HashMap<>();
+        Map<Integer, String[]> paperId2Authors = new HashMap<>();
 
-        while((line = br.readLine()) != null){
-            if (patternPaperTitle.matcher(line).find()){
+        FileWriter titleWriter = new FileWriter("title_year.tsv");
+        FileWriter paperConferenceWriter = new FileWriter("paper_conference_label.tsv");
+        FileWriter paperCitationWriter = new FileWriter("paper_citation_edge.tsv");
+        FileWriter authorConferenceWriter = new FileWriter("author_conference_label.tsv");
+
+        while ((line = br.readLine()) != null) {
+            if (patternPaperTitle.matcher(line).find()) {
                 paperTitle = line.substring(2);
-            }else if (patternPaperIndex.matcher(line).find()){
+            } else if (patternPaperIndex.matcher(line).find()) {
                 paperId = Integer.parseInt(line.substring(6));
-                titleWriter.write(paperId + " " + publishedYear + " " + paperTitle + "\n");
-            }else if(patternYear.matcher(line).find()){
+            } else if (patternYear.matcher(line).find()) {
                 publishedYear = Integer.parseInt(line.substring(2));
-            }else if (patterCitedPaper.matcher(line).find()){
-                citationWriter.write(paperId + " " + Integer.parseInt(line.substring(2)) + " " + publishedYear + "\n");
-            }else if(line.equals("^\n")){
+            } else if (patternCitedPaper.matcher(line).find()) {
+                citationPapers.add(Integer.parseInt(line.substring(2)));
+            } else if (patternConference.matcher(line).find()) {
+                conferenceName = line.substring(2).toLowerCase();
+            } else if (patternAuthors.matcher(line).find()) {
+                authors = line.substring(2).split(",");
+            } else if (line.trim().isEmpty()) {
+                titleWriter.write(paperId + "\t" + publishedYear + "\t" + paperTitle + "\n");
+                paperConferenceWriter.write(paperId + "\t" + conferenceName + "\n");
+
+                if (!citationPapers.isEmpty()){
+                    for (int citePaper : citationPapers) {
+                        paperCitationWriter.write(paperId + "\t" + citePaper + "\n");
+                    }
+                }
+
+                if (authors != null) {
+                    for (String authorName : authors) {
+                        Set<String> conferences = authorBelongs2Conferences.getOrDefault(authorName, new HashSet<>());
+                        conferences.add(conferenceName);
+                        authorBelongs2Conferences.put(authorName, conferences);
+                    }
+                }
+
+                paperId2Authors.put(paperId, authors);
+
+                paperTitle = "";
+                conferenceName = "";
                 paperId = 0;
                 publishedYear = 0;
-                paperTitle = "";
+                citationPapers.clear();
+                authors = null;
             }
         }
         titleWriter.close();
-        citationWriter.close();
+        paperCitationWriter.close();
+
+        System.out.println("Create authpr citation network");
+        for (Map.Entry<String, Set<String>> e : authorBelongs2Conferences.entrySet()) {
+            String author = e.getKey();
+            authorConferenceWriter.write(author);
+            for (String conference : e.getValue()) {
+                authorConferenceWriter.write("\t" + conference);
+            }
+            authorConferenceWriter.write("\n");
+        }
+
+        authorConferenceWriter.close();
+
+        // create author-citation network
+        FileWriter authorCitationWriter = new FileWriter("author_citation_edge.tsv");
+        Map<String, HashMap<String, Integer>> adjList = new HashMap<>();
+
+        br = new BufferedReader(new FileReader("./paper_citation_edge.tsv"));
+        while ((line = br.readLine()) != null) {
+            String[] edge = line.split("\t");
+            int fromPaper = Integer.parseInt(edge[0]);
+            int toPaper = Integer.parseInt(edge[1]);
+
+            for (String fromAuthor : paperId2Authors.getOrDefault(fromPaper, new String[0])) {
+                HashMap<String, Integer> authorCounter = adjList.getOrDefault(fromAuthor, new HashMap<>());
+                for (String toAuthor : paperId2Authors.get(toPaper)) {
+                    int w = authorCounter.getOrDefault(toAuthor, 0) + 1;
+                    authorCounter.put(toAuthor, w);
+                }
+                adjList.put(fromAuthor, authorCounter);
+            }
+        }
+
+        for (Map.Entry<String, HashMap<String, Integer>> e : adjList.entrySet()) {
+            String fromAuthor = e.getKey();
+            for (Map.Entry<String, Integer> counter : e.getValue().entrySet()) {
+                String toAuthor = counter.getKey();
+                int w = counter.getValue();
+                authorCitationWriter.write(fromAuthor + "\t" + toAuthor + "\t" + w + "\n");
+            }
+        }
+        authorCitationWriter.close();
     }
 }
